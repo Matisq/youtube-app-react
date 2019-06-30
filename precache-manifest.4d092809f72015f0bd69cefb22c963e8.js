4f933fc7a9fd03b635f64b5b2f0a5627e2f7301f
Matisq <mateusz942@interia.pl>
2019-07-01 01:39:45 +0200

    Updates

---
+++ precache-manifest.4d092809f72015f0bd69cefb22c963e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97472702263553cec74f5e7bd9a9faa9",
+    "url": "/youtube-app-react/index.html"
+  },
+  {
+    "revision": "bc5662f82a601fb568a8",
+    "url": "/youtube-app-react/static/css/main.36bf6d8b.chunk.css"
+  },
+  {
+    "revision": "57a1e51cbe9d30cab4ab",
+    "url": "/youtube-app-react/static/js/2.967770f2.chunk.js"
+  },
+  {
+    "revision": "bc5662f82a601fb568a8",
+    "url": "/youtube-app-react/static/js/main.f5b9cff5.chunk.js"
+  },
+  {
+    "revision": "2a4f09f0579a65069b88",
+    "url": "/youtube-app-react/static/js/runtime~main.f375142e.js"
+  }
+]);

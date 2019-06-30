5b7fbce29ac733e2303d8a61970c7ea8a4b3e122
Matisq <mateusz942@interia.pl>
2019-06-30 21:12:41 +0200

    Add event handlers and state for handling user input

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,23 @@ import {VideoDetails} from './VideoDetails/VideoDetails';
 import {VideoList} from './VideoList/VideoList';
 
 const App = () => {
+    const [videoName, setVideoName] = useState('Marvel');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        if(!videoName.trim().length) return;
+        setVideoName('');
+    
+    }
+    
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setVideoName(value);
+    }
+    
     return (
         <Container>
-            <SearchVideoBar/>
+             <SearchVideoBar handleChange={handleChange} handleFormSubmit={handleFormSubmit} videoName={videoName}/>
             <div className="row mt-5">
                 <div className="col-12 col-lg-8">
                     <VideoDetails/>

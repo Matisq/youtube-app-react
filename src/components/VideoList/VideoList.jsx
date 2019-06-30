ab3d541f49d32b5d9bf5f61300b4826369c0e796
Matisq <mateusz942@interia.pl>
2019-06-30 23:44:17 +0200

    Render list of videos as VideoItem component

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,13 +1,15 @@
 import React, {Fragment} from 'react';
 import VideoItem from '../VideoItem/VideoItem';
 
-export const VideoList = () => {
+export const VideoList = ({videos}) => {
+    const videosList = videos.map(video => (<VideoItem key={video.etag} video={video}/>));
     return (
         <Fragment>
             <div className="row">
                 <p style={{
                     fontSize: '1rem'
                 }}>Next</p>
+                {videosList}
             </div>
         </Fragment>
     )
